56b27f750a9273a0ff00fae37bbfc7477cf134b7
Bailey-redbrick <bailey@redbrick.space>
2023-08-16 16:26:04 +0900

    Chore: set tailwind and modify modal

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -7,14 +7,30 @@ function page() {
     <div>
       <Dialog.Root>
         <Dialog.Trigger>
-          <button>open modal</button>
+          <button className="rounded p-2 bg-slate-100 ">open modal</button>
         </Dialog.Trigger>
+
         <Dialog.Portal>
-          <Dialog.Overlay />
-          <Dialog.Content>
-            <Dialog.Title />
-            <Dialog.Description />
-            <Dialog.Close />
+          <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+          <Dialog.Content className="p-5 fixed flex items-center justify-between left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white w-96 text-gray-900 shadow">
+            <div>
+              <Dialog.Title>Title</Dialog.Title>
+              <Dialog.Description>Description</Dialog.Description>
+            </div>
+            <Dialog.Close className="text-gray-400 hover:text-gray-500">
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </Dialog.Close>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
