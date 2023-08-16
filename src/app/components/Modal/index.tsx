535d4d17f19254628eb1c2befb80e9b9814d4b78
Bailey-redbrick <bailey@redbrick.space>
2023-08-16 17:10:47 +0900

    Refactor: use Modal component to handle reuseable modals

---
+++ src/app/components/Modal/index.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+
+interface Props {
+  open: boolean;
+  onOpenChange: () => void;
+  children: ReactNode;
+}
+
+function Modal({ open, onOpenChange, children }: Props) {
+  return (
+    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+      {children}
+    </Dialog.Root>
+  );
+}
+
+function ModalContent({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) {
+  return (
+    <Dialog.Portal>
+      <Dialog.Overlay className="fixed inset-0 bg-black/50 data-[state=closed]:animate-[dialog-overlay-hide_200ms] data-[state=open]:animate-[dialog-overlay-show_200ms]" />
+      <Dialog.Content className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms]">
+        <div className="flex items-center justify-between">
+          <Dialog.Title className="text-xl">{title}</Dialog.Title>
+          <Dialog.Close className="text-gray-400 hover:text-gray-500">
+            close
+          </Dialog.Close>
+        </div>
+
+        {children}
+      </Dialog.Content>
+    </Dialog.Portal>
+  );
+}
+
+Modal.Button = Dialog.Trigger;
+Modal.Close = Dialog.Close;
+Modal.Content = ModalContent;
+
+export default Modal;
