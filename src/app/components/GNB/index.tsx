4e4437cef7348a67400c9d2bc31566f161b4fd40
Bailey-redbrick <bailey@redbrick.space>
2023-08-22 17:21:10 +0900

    Modify: gnb buttons

--- src/app/components/GNB/index.tsx
+++ src/app/components/GNB/index.tsx
@@ -1,70 +1,39 @@
 "use client";
-import React, { ReactComponentElement, ReactNode } from "react";
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import Link from "next/link";
+import React, { ReactNode } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { SlSocialGithub } from "react-icons/sl";
 import { GoProjectRoadmap } from "react-icons/go";
-import { CaretDownIcon } from "@radix-ui/react-icons";
-import { IconType } from "react-icons";
+import { BsBook } from "react-icons/bs";
+
 function Gnb() {
   return (
-    <div className="fixed bottom-10 pl-8 pr-8 flex justify-center items-center gap-x-8 h-12 bg-slate-primary rounded-primary shadow-new-morph">
-      <NavigationMenu.Root className="NavigationMenuRoot">
-        <NavigationMenu.List className="flex gap-x-5">
-          <GnbItem Button={AiOutlineHome} href="/"></GnbItem>
+    <div className="fixed bottom-5 pl-8 pr-8 flex justify-between items-center min-w-min w-1/3 h-12 bg-slate-primary/50 rounded-primary">
+      <GnbButton href="/">
+        <AiOutlineHome />
+      </GnbButton>
 
-          <GnbItem Button={GoProjectRoadmap}>
-            <ul className="flex flex-col">
-              <Link href="/portfolio">portfolio</Link>
+      <GnbButton href="https://github.com/Gayun00">
+        <SlSocialGithub />
+      </GnbButton>
 
-              <Link href="/posts">Blog</Link>
-            </ul>
-          </GnbItem>
-
-          <GnbItem Button={SlSocialGithub} href={"/"}>
-            <ul className="List two">
-              <Link href="/portfolio">portfolio</Link>
-              <Link href="/posts">Blog</Link>
-            </ul>
-          </GnbItem>
-
-          <NavigationMenu.Indicator className="NavigationMenuIndicator">
-            <div className="Arrow" />
-          </NavigationMenu.Indicator>
-        </NavigationMenu.List>
-
-        {/* <div className="ViewportPosition">
-          <NavigationMenu.Viewport className="NavigationMenuViewport" />
-        </div> */}
-      </NavigationMenu.Root>
+      <GnbButton href="/portfolio">
+        <GoProjectRoadmap />
+      </GnbButton>
+      <GnbButton href="/posts">
+        <BsBook />
+      </GnbButton>
     </div>
   );
 }
 
-interface GnbItemProps {
-  Button: IconType;
-  children?: ReactNode;
-  href?: string;
-}
-
-function GnbItem({ Button, href, children }: GnbItemProps) {
+function GnbButton({ children, href }: { children: ReactNode; href: string }) {
   return (
-    <NavigationMenu.Item>
-      <NavigationMenu.Trigger className="flex">
-        {href ? (
-          <Link href={href}>
-            <Button />
-          </Link>
-        ) : (
-          <Button />
-        )}
-        {children && <CaretDownIcon className="CaretDown" aria-hidden />}
-      </NavigationMenu.Trigger>
-      <NavigationMenu.Content className="absolute top-10">
-        {children}
-      </NavigationMenu.Content>
-    </NavigationMenu.Item>
+    <Link
+      className="flex items-center justify-center bg-slate-200 w-10 h-10 rounded-primary"
+      href={href}>
+      {children}
+    </Link>
   );
 }
 
