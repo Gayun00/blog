e139700fc73f5a1533aad3d13115f23ff5086a83
Bailey-redbrick <bailey@redbrick.space>
2023-08-22 17:53:06 +0900

    Modify: post page
    - edit example post md file
    - add style of title, content, thumbnail

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getFileFromFolder } from "@/utils";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
+import Image from "next/image";
 
 export async function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }];
@@ -20,11 +21,18 @@ export default async function Page({ params }: { params?: { id: string } }) {
   const { data, content } = await getPostContent(params?.id);
 
   return (
-    <div className="space-y-4">
-      <h1 className="text-2xl font-medium text-gray-100">{data.title}</h1>
-      <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
-        {content}
-      </ReactMarkdown>
+    <div className="pt-16 flex flex-col items-center">
+      <Image src={data.thumbnail} alt="post_thumbnail" width={300} height={0} />
+      <main className="mt-10">
+        <h1 className="mb-10 text-2xl font-medium text-gray-primary">
+          {data.title}
+        </h1>
+        <ReactMarkdown
+          className="prose lg:prose-xl"
+          remarkPlugins={[remarkGfm]}>
+          {content}
+        </ReactMarkdown>
+      </main>
     </div>
   );
 }
