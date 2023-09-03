2f8d1f469106409b84f72e1478f514a3297680ff
Gayun00 <gayunlee11@gmail.com>
2023-09-03 23:16:51 +0900

    Chore: add react-three

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <div id="canvas-container">
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
+      </div>
     </main>
   );
 }
