2d88edd0e4015d14f9eed77598525ed922b84070
Gayun00 <gayunlee11@gmail.com>
2023-09-04 00:23:24 +0900

    Add: cube drop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,53 @@
 "use client";
-
-import { ReactNode, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import {
-  Edges,
-  MeshPortalMaterial,
-  CameraControls,
-  Environment,
-  PivotControls,
-} from "@react-three/drei";
-import { Group } from "three";
-import { useControls } from "leva";
+import { useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 
 export default function Home() {
+  const [ready, set] = useState(false);
+  useEffect(() => {
+    const timeout = setTimeout(() => set(true), 1000);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
-    <main className="h-screen">
-      <Canvas shadows camera={{ position: [-3, 0.5, 3] }}>
-        <mesh castShadow receiveShadow>
-          <boxGeometry args={[2, 2, 2]} />
-          <Edges />
-          <Side rotation={[0, 0, 0]} bg="orange" index={0}>
-            <torusGeometry args={[0.65, 0.3, 64]} />
-          </Side>
-          <Side rotation={[0, Math.PI, 0]} bg="lightblue" index={1}>
-            <torusKnotGeometry args={[0.55, 0.2, 128, 32]} />
-          </Side>
-          <Side
-            rotation={[0, Math.PI / 2, Math.PI / 2]}
-            bg="lightgreen"
-            index={2}>
-            <boxGeometry args={[1.15, 1.15, 1.15]} />
-          </Side>
-          <Side
-            rotation={[0, Math.PI / 2, -Math.PI / 2]}
-            bg="aquamarine"
-            index={3}>
-            <octahedronGeometry />
-          </Side>
-          <Side rotation={[0, -Math.PI / 2, 0]} bg="indianred" index={4}>
-            <icosahedronGeometry />
-          </Side>
-          <Side rotation={[0, Math.PI / 2, 0]} bg="hotpink" index={5}>
-            <dodecahedronGeometry />
-          </Side>
-        </mesh>
-        <CameraControls makeDefault />
+    <div className="h-screen ">
+      <Canvas dpr={[1, 2]} shadows camera={{ position: [-5, 5, 5], fov: 50 }}>
+        <ambientLight color="white" />
+        <ambientLight color="white" intensity={0.1} />
+        <directionalLight color="white" position={[5, 5, 5]} />
+        <spotLight
+          angle={0.25}
+          penumbra={0.5}
+          position={[10, 10, 5]}
+          castShadow
+        />
+        <Physics>
+          <Plane />
+          <Cube position={[0, 5, 0]} />
+          <Cube position={[0.45, 7, -0.25]} />
+          <Cube position={[-0.45, 9, 0.25]} />
+          {ready && <Cube position={[-0.45, 10, 0.25]} />}
+        </Physics>
       </Canvas>
-    </main>
+    </div>
+  );
+}
+function Plane(props: any) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+  return (
+    <mesh receiveShadow ref={ref}>
+      <planeGeometry args={[1000, 1000]} />
+      <meshStandardMaterial color="#f0f0f0" />
+    </mesh>
   );
 }
 
-interface SideProps {
-  rotation: [x: number, y: number, z: number];
-  bg: string;
-  children: ReactNode;
-  index: number;
-}
-
-function Side({
-  rotation = [0, 0, 0],
-  bg = "#f0f0f0",
-  children,
-  index,
-}: SideProps) {
-  const groupRef = useRef<Group>(null);
-  const { worldUnits } = useControls({ worldUnits: false });
-  useFrame((state, delta) => {
-    if (groupRef.current) {
-      groupRef.current.rotation.x += delta;
-      groupRef.current.rotation.y += delta;
-    }
-  });
-  const boxSize: [width: number, height: number, depth: number] = [2, 2, 2];
-
+function Cube(props: any) {
+  const [ref] = useBox(() => ({ mass: 1, ...props }));
   return (
-    <MeshPortalMaterial worldUnits={worldUnits} attach={`material-${index}`}>
-      <ambientLight intensity={0.5} />
-      <Environment preset="city" />
-
-      <group ref={groupRef}>
-        <mesh castShadow receiveShadow rotation={rotation}>
-          <meshStandardMaterial aoMapIntensity={1} color={bg} />{" "}
-          <spotLight
-            castShadow
-            color={bg}
-            intensity={2}
-            position={[10, 10, 10]}
-            angle={0.15}
-            penumbra={1}
-            shadow-normalBias={0.05}
-            shadow-bias={0.0001}
-          />
-        </mesh>
-        <mesh castShadow receiveShadow>
-          {children}
-          <meshLambertMaterial color={bg} />
-        </mesh>
-      </group>
-    </MeshPortalMaterial>
+    <mesh castShadow ref={ref}>
+      <boxGeometry />
+      <meshStandardMaterial color="orange" />
+    </mesh>
   );
 }
